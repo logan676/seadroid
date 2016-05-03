c8ed41c324359407ab9b1cce66b4471c45bed40a
Logan <logan676395@gmail.com>
2016-05-03 11:03:34 +0800

    Add email validation test

--- app/src/test/java/com/seafile/seadroid2/util/UtilsTest.java
+++ app/src/test/java/com/seafile/seadroid2/util/UtilsTest.java
@@ -1,7 +1,7 @@
 package com.seafile.seadroid2.util;
 
-import org.junit.Test;
 import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 
@@ -32,5 +32,25 @@ public class UtilsTest {
 
         result = Utils.pathJoin("/a/", "/b/c/", "/d/e");
         Assert.assertEquals("/a/b/c/d/e", result);
+    }
+
+    @Test
+    public void testEmailValid() {
+        boolean valid = Utils.isValidEmail("test@test123.com");
+        Assert.assertEquals(valid, true);
+        valid = Utils.isValidEmail("test123@test123.com");
+        Assert.assertEquals(valid, true);
+        valid = Utils.isValidEmail("test123_abc@test123.com");
+        Assert.assertEquals(valid, true);
+        valid = Utils.isValidEmail("test.tset@test123.com");
+        Assert.assertEquals(valid, true);
+        valid = Utils.isValidEmail("test@test123.com.cn");
+        Assert.assertEquals(valid, true);
+        valid = Utils.isValidEmail("test@test123_abc.com");
+        Assert.assertEquals(valid, false);
+        valid = Utils.isValidEmail("test_test123.com.cn");
+        Assert.assertEquals(valid, false);
+        valid = Utils.isValidEmail("test@test123");
+        Assert.assertEquals(valid, false);
     }
 }
